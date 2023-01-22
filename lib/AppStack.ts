b9c3fe1689f3c8bc4e0603569e7da9281f58676f
Joao Romao <jrmromao@gmail.com>
2023-01-22 12:43:09 +0000

    Update stack file
    - Added to code to add dynamoDB tables for role & department
    - updated dir structure to make lambda creation more dynamic

--- lib/AppStack.ts
+++ lib/AppStack.ts
@@ -1,24 +1,28 @@
 import * as cdk from "aws-cdk-lib";
 import { CfnOutput } from "aws-cdk-lib";
-import { AuthorizationType, Cors, MethodOptions, ResourceOptions, RestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+  AuthorizationType,
+  Cors,
+  MethodOptions,
+  ResourceOptions,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
 import { CloudFrontWebDistribution } from "aws-cdk-lib/aws-cloudfront";
-
 import { Bucket, HttpMethods } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
 import { join } from "path";
 import { AuthorizerWrapper } from "./auth/AuthorizerWrapper";
 import { config } from "./config/configuration";
-import { GenericTable } from "./GenericTable";
+import { GenericTable } from "./utils/GenericTable";
 import { Policies } from "./Policies";
-
 
 export class AppStack extends cdk.Stack {
   private suffix: string;
 
   private deploymentBucket: Bucket;
   private documentBucket: Bucket;
-  
+
   private api = new RestApi(this, "HRAppAPI");
   private authorizer: AuthorizerWrapper;
   private policies: Policies;
@@ -30,7 +34,30 @@ export class AppStack extends cdk.Stack {
     readLambdaPath: "Read",
     updateLambdaPath: "Update",
     deleteLambdaPath: "Delete",
+    servicePath: "EmployeeLambdas",
     secondaryIndexes: ["user"],
+  });
+
+  private officeTable = new GenericTable(this, {
+    tableName: "OfficeTable",
+    primaryKey: "officeId",
+    createLambdaPath: "Create",
+    readLambdaPath: "Read",
+    updateLambdaPath: "Update",
+    deleteLambdaPath: "Delete",
+    servicePath: "OfficeLambdas",
+    secondaryIndexes: ["name"],
+  });
+
+  private departmentTable = new GenericTable(this, {
+    tableName: "DepartmentTable",
+    primaryKey: "departmentId",
+    createLambdaPath: "Create",
+    readLambdaPath: "Read",
+    updateLambdaPath: "Update",
+    deleteLambdaPath: "Delete",
+    servicePath: "DepartmentLambdas",
+    secondaryIndexes: ["name"],
   });
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -39,13 +66,11 @@ export class AppStack extends cdk.Stack {
     this.initializeDocumentBucket();
 
 
-    const bucketName = "app.jrmromao.com";
-
     this.deploymentBucket = new Bucket(this, "hr-app-client-ui", {
-      bucketName: bucketName,
+      bucketName: config.uiBucketName,
       publicReadAccess: true,
       websiteIndexDocument: "index.html",
-      websiteErrorDocument: 'index.html',
+      websiteErrorDocument: "index.html",
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
@@ -71,26 +96,21 @@ export class AppStack extends cdk.Stack {
 
     this.policies = new Policies(this.documentBucket);
 
+    this.authorizer = new AuthorizerWrapper(this, this.api, this.policies);
 
-    this.authorizer = new AuthorizerWrapper(
-        this,
-        this.api,
-        this.policies);
- 
+    const optionsWithAuthorizer: MethodOptions = {
+      authorizationType: AuthorizationType.COGNITO,
+      authorizer: {
+        authorizerId: this.authorizer.authorizer.authorizerId,
+      },
+    };
 
-        const optionsWithAuthorizer: MethodOptions = {
-          authorizationType: AuthorizationType.COGNITO,
-          authorizer: {
-              authorizerId: this.authorizer.authorizer.authorizerId
-          }
-      }
-      
-  const optionsWithCors:ResourceOptions = {
-      defaultCorsPreflightOptions : {
-          allowOrigins: Cors.ALL_ORIGINS,
-          allowMethods: Cors.ALL_METHODS
-      }
-  }
+    const optionsWithCors: ResourceOptions = {
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+    };
 
     new BucketDeployment(this, "hr-app-client-ui-deployment", {
       destinationBucket: this.deploymentBucket,
@@ -107,7 +127,7 @@ export class AppStack extends cdk.Stack {
       value: hrAppCloudFront.distributionDomainName,
     });
 
-    //Reservations API integrations:
+    //Employee API integrations:
     const reservationResource = this.api.root.addResource(
       "api",
       optionsWithCors
@@ -132,6 +152,53 @@ export class AppStack extends cdk.Stack {
       this.employeeTable.deleteLambdaIntegration,
       optionsWithAuthorizer
     );
+    //Office API integrations:
+    const officeResource = this.api.root.addResource("api", optionsWithCors);
+    reservationResource.addMethod(
+      "POST",
+      this.officeTable.createLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    reservationResource.addMethod(
+      "GET",
+      this.officeTable.readLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    reservationResource.addMethod(
+      "PUT",
+      this.officeTable.updateLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    reservationResource.addMethod(
+      "DELETE",
+      this.officeTable.deleteLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    //Department API integrations:
+    const departmentResource = this.api.root.addResource(
+      "api",
+      optionsWithCors
+    );
+    reservationResource.addMethod(
+      "POST",
+      this.departmentTable.createLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    reservationResource.addMethod(
+      "GET",
+      this.departmentTable.readLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    reservationResource.addMethod(
+      "PUT",
+      this.departmentTable.updateLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    reservationResource.addMethod(
+      "DELETE",
+      this.departmentTable.deleteLambdaIntegration,
+      optionsWithAuthorizer
+    );
   }
 
   private initializeSuffix() {
@@ -141,20 +208,26 @@ export class AppStack extends cdk.Stack {
   }
 
   private initializeDocumentBucket() {
-    this.documentBucket = new Bucket(this, `document-bucket-${config.environment}`, {
+    this.documentBucket = new Bucket(
+      this,
+      `document-bucket-${config.environment}`,
+      {
         bucketName: `document-bucket-${this.suffix}`,
-        cors: [{
+        cors: [
+          {
             allowedMethods: [
-                HttpMethods.HEAD,
-                HttpMethods.GET,
-                HttpMethods.PUT
+              HttpMethods.HEAD,
+              HttpMethods.GET,
+              HttpMethods.PUT,
             ],
-            allowedOrigins: ['*'],
-            allowedHeaders: ['*']
-        }]
+            allowedOrigins: ["*"],
+            allowedHeaders: ["*"],
+          },
+        ],
+      }
+    );
+    new CfnOutput(this, "document-bucket-name", {
+      value: this.documentBucket.bucketName,
     });
-    new CfnOutput(this, 'document-bucket-name', {
-        value: this.documentBucket.bucketName
-    })
-}
+  }
 }
